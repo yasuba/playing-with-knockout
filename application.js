951ff2a4afe6a897d54631ef321c044708c5b991
Maya Driver <mayadrivermd@gmail.com>
2015-02-05 13:18:58 +0000

    added day of week logic

--- application.js
+++ application.js
@@ -1,21 +1,25 @@
 $(document).ready(function(){
 
   function viewModel() {
-
     var self = this
-   self.dayOfWeek = ko.observable('Wednesday');
-   self.activity = ko.observable('work');
-   self.day = ko.observable('04');
-   self.month = ko.observable('02');
-   self.year = ko.observable('2015');
+    var date = new Date();
+    var d = date.getDay() - 1;
+    var n = date.getDate();
+    var m = date.getMonth() + 1;
+    self.daysOfWeek = ko.observableArray(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']);
+    self.dayOfWeek = ko.observable(self.daysOfWeek()[d]);
+    self.activity = ko.observable('work');
+    self.day = ko.observable(n);
+    self.month = ko.observable(m);
+    self.year = ko.observable('2015');
 
-   self.fullDate = ko.computed(function(){
+    console.log(date)
+
+    self.fullDate = ko.computed(function(){
     return self.day() + '/' + self.month() + '/' + self.year();
-   });
+    });
 
-   self.daysOfWeek = ko.observableArray(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']);
-   alert("The week has " + self.daysOfWeek().length + ' days');
-  };
+    };
 
   ko.applyBindings(new viewModel());
 
