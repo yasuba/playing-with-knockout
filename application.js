56dc1bba78150ed3351ce742cb12028d719e4bf3
Maya Driver <mayadrivermd@gmail.com>
2015-02-05 13:30:57 +0000

    added random activity generator

--- application.js
+++ application.js
@@ -6,14 +6,15 @@ $(document).ready(function(){
     var d = date.getDay() - 1;
     var n = date.getDate();
     var m = date.getMonth() + 1;
+    var y = date.getFullYear();
     self.daysOfWeek = ko.observableArray(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']);
+    self.activities = ko.observableArray(['work', 'fun', 'sleep', 'drink!']);
     self.dayOfWeek = ko.observable(self.daysOfWeek()[d]);
-    self.activity = ko.observable('work');
+    var rand = Math.floor((Math.random() * (4 - 0)));
+    self.activity = ko.observable(self.activities()[rand]);
     self.day = ko.observable(n);
     self.month = ko.observable(m);
-    self.year = ko.observable('2015');
-
-    console.log(date)
+    self.year = ko.observable(y);
 
     self.fullDate = ko.computed(function(){
     return self.day() + '/' + self.month() + '/' + self.year();
